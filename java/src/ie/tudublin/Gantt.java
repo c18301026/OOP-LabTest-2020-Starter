88eedc536d8f21a0be9a79bfc6d014e29f5f7807
c18301026 <c18301026@mytudublin.ie>
2020-04-22 12:49:40 +0100

    Displayed task names on the left

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -51,11 +51,21 @@ public class Gantt extends PApplet {
 		float txtBord = width / 5;
 		textAlign(CENTER, CENTER);
 		stroke(255);
+		fill(255);
 
 		for(int i = 1; i < 31; i++) {
 			float x = map(i, 1, 30, txtBord, width - Bord);
 			line(x, Bord, x, height - Bord);
 			text(i, x, Bord / 2);
+		}
+
+		textAlign(LEFT, CENTER);
+
+		for(Task ts : tasks) {
+			float txtX = txtBord / 4;
+			float txtY = map(tasks.indexOf(ts), 0, tasks.size(), Bord * 2, height - txtBord);
+
+			text(ts.getTask(), txtX, txtY);
 		}
 	}
 
